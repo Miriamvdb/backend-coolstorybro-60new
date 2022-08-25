c01c6d4965c5a97f36afd63403c86f9bc6bb00f9
Miriamvdb <miriamvdb@live.nl>
2022-08-25 13:47:09 +0200

    Back/F2: GET space by id, include stories

--- routers/space.js
+++ routers/space.js
@@ -1,5 +1,6 @@
 const { Router } = require("express");
 const Space = require("../models").space;
+const Story = require("../models").story;
 const router = new Router();
 
 // F1 - GET all spaces - http GET :4000/spaces
@@ -9,6 +10,21 @@ router.get("/", async (req, res, next) => {
     res.send(allSpaces);
   } catch (e) {
     console.log(e.message);
+    next(e);
+  }
+});
+
+// F2 - GET space by id, include stories - http GET :4000/spaces/1
+router.get("/:id", async (req, res, next) => {
+  try {
+    const id = parseInt(req.params.id);
+    const spaceById = await Space.findByPk(id, { include: [Story] });
+    if (spaceById) {
+      res.send(spaceById);
+    } else {
+      res.status(404).send({ message: "Space not found!" });
+    }
+  } catch (e) {
     next(e);
   }
 });
