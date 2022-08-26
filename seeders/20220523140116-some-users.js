acdac480c5a20fea6fc4631bd4d8779078ad235f
Miriamvdb <miriamvdb@live.nl>
2022-08-26 10:56:07 +0200

    MATIAS FIXED: bcrypt.hashSync password - in seeders

--- seeders/20220523140116-some-users.js
+++ seeders/20220523140116-some-users.js
@@ -1,4 +1,5 @@
 "use strict";
+const bcrypt = require("bcrypt");
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
@@ -9,26 +10,26 @@ module.exports = {
           name: "Apple",
           email: "apple@apple.com",
           phone: 1234567,
-          password: "apple",
+          password: bcrypt.hashSync("apple", 10),
           createdAt: new Date(),
           updatedAt: new Date(),
-        }, 
+        },
         {
           name: "Banana",
           email: "banana@banana.com",
           phone: 1234567,
-          password: "banana",
+          password: bcrypt.hashSync("banana", 10),
           createdAt: new Date(),
           updatedAt: new Date(),
-        },  
+        },
         {
           name: "Coco",
           email: "coco@coco.com",
           phone: 1234567,
-          password: "coco",
+          password: bcrypt.hashSync("coco", 10),
           createdAt: new Date(),
           updatedAt: new Date(),
-        },     
+        },
       ],
       {}
     );
